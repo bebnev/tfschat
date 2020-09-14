46e236cc28424081235d50c1e7001257ab8d98e5
Bebnev Anton <bambr.ab@gmail.com>
2020-09-15 02:51:10 +0300

    add log to application(:) method

--- Chat/AppDelegate.swift
+++ Chat/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var firstLaunch = true
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        log("Application is almost ready to run")
         return true
     }
     
