65c06df398eb5745aad47b9924d5b8b1d822b2b5
Bebnev Anton <bambr.ab@gmail.com>
2020-09-28 23:44:18 +0300

    add navigation and controllers

---
+++ Chat/ConversationViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ConversationViewController.swift
+//  Chat
+//
+//  Created by Anton Bebnev on 28.09.2020.
+//  Copyright © 2020 Anton Bebnev. All rights reserved.
+//
+
+import UIKit
+
+class ConversationViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.blue
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
